c62c6f26d73257c2c7fd884ce0ee579b8b248b35
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-10-15 14:15:18 +0800

    added onInputChange event

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 
 class SearchBar extends React.Component {
+    state = { text: '' };
+
+
+    onInputChange = e => {
+        console.log(e.target.value);
+
+        this.setState({ text: e.target.value });
+    }
 
     render() {
         return (
@@ -9,7 +17,7 @@ class SearchBar extends React.Component {
                 <form>
                     <div className="col-12 col-md-8">
                         <label>Search Images</label>
-                        <input type="text" name="search" className="form-control form-control-sm" />
+                        <input onChange={this.onInputChange} type="text" name="search" className="form-control form-control-sm" />
                     </div>
                 </form>
             </div>
