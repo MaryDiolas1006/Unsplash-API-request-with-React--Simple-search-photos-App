896fad0e8c9f07a137f622a1c546e3c2d0b894b4
Mary Ruth Diolas <mdiosgalla@gmail.com>
2020-10-15 14:23:50 +0800

    added asyn await syntax

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
 
-  onSearchSubmit = text => {
-    axios.get('https://api.unsplash.com/search/photos', {
+  state = { images: [] };
+
+  onSearchSubmit = async text => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {query: text},
       headers: {
         Authorization: 'Client-ID Erva7o5Z_A5ZkgEiy48g_opci9k3a6ZRuwPUzwpIb3M'
       }
     })
+    this.setState({ images: response.data.results });
   }
 
   render() {
